7789c308a62f0c5d0c50841421cdd245e4a8766a
lsundarkavitha <57778566+lsundarkavitha@users.noreply.github.com>
2019-11-15 15:32:18 -0500

    Add files via upload
    This is the normal string program where I want to print the given name. But I am getting an error in line 6.

---
+++ String.java
@@ -0,0 +1,13 @@
+package test3;
+
+public class String {
+
+	public static void main(String[] args) {
+		printnamestatic("Kiran");
+
+	}
+	//static
+	public static void printnamestatic(String text) {  
+		System.out.println(text);
+	}
+}
