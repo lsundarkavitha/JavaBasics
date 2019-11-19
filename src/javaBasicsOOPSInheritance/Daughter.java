701f0130e95226c7591de8f5f50a139cbe4a5d88
Prem Kumar <kpremkumar@allegisgroup.com>
2019-11-18 20:18:23 -0500

    Create Daughter.java
    inheritance

---
+++ src/javaBasicsOOPSInheritance/Daughter.java
@@ -0,0 +1,11 @@
+package javaBasicsOOPSInheritance;
+
+public class Daughter extends Father{
+
+    public static void main(String[] args) {
+
+        Daughter d= new Daughter();
+        d.method1();
+        d.method3();
+    }
+}
